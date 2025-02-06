1232956213d7031d68e9c9a1f01ad981298940f8
dhruvghinaiya12 <dhruvghinaiya12@gmail.com>
2025-02-06 14:07:46 +0530

    React

---
+++ task management system/client/src/pages/AssignTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AssignTask = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AssignTask
---
+++ task management system/server/routes/index.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const cors=require("cors");
+const db = require("../config/db");
+
+require("dotenv").config();
+
+const port=process.env.PORT || 5050
+const app=express()
+
+app.use(cors({
+    origin: "http://localhost:5173", 
+}));
+app.use(express.json())
+app.get("/",(req, res) =>{
+    res.send("Hello World!")
+})
+
+// app.use("/api/v1",AppRoutes)
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+   db();
+})
