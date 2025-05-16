b18e394f0c85acd0e4596fca26fff96f6f211621
dhruvghinaiya12 <dhruvghinaiya12@gmail.com>
2025-05-16 15:23:31 +0530

    React

--- task management system/server/middleware/CheckToken.js
+++ task management system/server/middleware/CheckToken.js
@@ -1,30 +1,26 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const Token=async(req,res,next)=>{
-    const PublicRoute=["/user/signup","/user/login"]
+const Token = async (req, res, next) => {
+    const PublicRoute = ["/user/signup", "/user/login", "/user/verify"];
 
-    let url=req.url
-    if(url.includes("/user/verify")){
+    if (PublicRoute.some(route => req.url.includes(route))) {
         return next();
     }
 
-    if(PublicRoute.includes(req.url)){
-        return next();
-    }
     let token = req.headers.authorization?.split(' ')[1];
     
-    if(!token){
-        return res.status(401).json({message: "Token is required"});
+    if (!token) {
+        return res.status(401).json({ message: "Token is required" });
     }
     
     try {
-        let Decode=await jwt.verify(token, process.env.SECRET_KEY);
-        req.user=Decode;
+        let Decode = await jwt.verify(token, process.env.SECRET_KEY);
+        req.user = Decode;
         next();
     } catch (error) {
-        return res.status(401).json({message: "You are not authorized"});
+        return res.status(401).json({ message: "You are not authorized" });
     }
 }
 
-module.exports=Token;
+module.exports = Token;
